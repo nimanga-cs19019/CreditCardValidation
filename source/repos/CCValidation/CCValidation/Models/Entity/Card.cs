96f7f98214643c50da953df7cc317c708e361a00
Pivindi Nimanga <nimanga-cs101901@stu.ac.lk>
2024-07-23 09:40:20 +0530

    first commit

---
+++ source/repos/CCValidation/CCValidation/Models/Entity/Card.cs
@@ -0,0 +1,11 @@
+﻿namespace CCValidation.Models.Entity
+{
+    public class Card
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible { get; set; }
+
+    }
+}
